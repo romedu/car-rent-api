8129c45bccf34e4d3a74b59f9b60980cbdec85a0
romedu <32285662+romedu@users.noreply.github.com>
2019-09-06 22:34:26 -0400

    Fix a typo in the auth routes file

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const router = require("express").Router,
+const router = require("express").Router(),
 	bcrypt = require("bcryptjs"),
 	jwt = require("jsonwebtoken"),
 	dbPool = require("../model"),
