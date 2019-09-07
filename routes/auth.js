cce36a57122765e1ccd398f15f4018f885459ca1
romedu <32285662+romedu@users.noreply.github.com>
2019-09-06 23:27:23 -0400

    Create the register route

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const router = require("express").Router(),
 
 router.post("/login", (req, res, next) => {
 	const { username, password } = req.body,
-		userQuery = `SELECT id, password FROM app_user WHERE username=${username}`;
+		userQuery = `SELECT id, password FROM client WHERE username=${username}`;
 
 	dbPool.query(userQuery, async (error, results) => {
 		try {
@@ -22,6 +22,32 @@ router.post("/login", (req, res, next) => {
 			next(error);
 		}
 	});
+});
+
+router.post("/register", async (req, res, next) => {
+	try {
+		const {
+				username,
+				password,
+				name,
+				nationalId,
+				cardNo,
+				isJuridic
+			} = req.body,
+			passwordHash = await bcrypt.hash(password, 8),
+			query = `
+            INSERT INTO client(username, password, name, nationalId, cardNo, isJuridic)
+            VALUES (${username}, ${passwordHash}, ${name}, ${nationalId}, ${cardNo}, ${isJuridic});
+         `;
+
+		dbPool.query(query, async (error, result) => {
+			if (error) throw error;
+			const { insertId: userId } = JSON.parse(JSON.stringify(result));
+			return res.status(201).json({ token: await getValidJwt(userId) });
+		});
+	} catch (error) {
+		return next(error);
+	}
 });
 
 const getValidJwt = async userId => await jwt.sign({ userId }, SECRET);
