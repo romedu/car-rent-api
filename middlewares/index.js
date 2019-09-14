65bab854547b732c5d74e7e725cdd10fa04ac1ee
romedu <32285662+romedu@users.noreply.github.com>
2019-09-13 22:51:50 -0400

    Add error handling to the checkIfToken middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,16 +1,21 @@
 const jwt = require("jsonwebtoken"),
-	{ SECRET } = process.env;
+	{ SECRET } = process.env,
+	{ createError } = require("../utils");
 
 // Checks if a valid token was passed within the request headers.
 // If valid, set the currentUserId to the req.locals object
 exports.checkIfToken = async (req, res, next) => {
 	try {
-		const token = req.get("Authorization"),
-			{ userId: currentUserId } = await jwt.verify(token, SECRET);
+		const token = req.get("Authorization");
 
-		req.locals = { currentUserId };
-		return next();
+		if (!token) throw createError(401);
+		else {
+			const { userId: currentUserId } = await jwt.verify(token, SECRET);
+			req.locals = { currentUserId };
+			return next();
+		}
 	} catch (error) {
+		if (!error.status) error = createError(400, "Invalid token");
 		next(error);
 	}
 };
