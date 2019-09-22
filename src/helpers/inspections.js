3339c93ce1afe7020ce78340cd77aa3cc14e7e87
romedu <32285662+romedu@users.noreply.github.com>
2019-09-22 13:31:43 -0400

    Refactor the findAll helper functions's query

--- src/helpers/inspections.js
+++ src/helpers/inspections.js
@@ -26,7 +26,8 @@ exports.findAll = (req, res, next) => {
                ON make.id = model.make_id
                ${whereClause}
                ORDER BY vehicle.rents
-               LIMIT ${page * 10}, ${page * 10 - 9};
+               LIMIT 10
+               OFFSET ${page * 10 - 10};
             `;
 
 	dbPool.query(query, (error, results) => {
